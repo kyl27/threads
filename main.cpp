d312ff2c24bcc3b14229bcbaa1e5d9357d68e11c
Kevin Li <kyl27@cornell.edu>
2016-09-06 00:15:01 -0400

    Added sequential benchmark

--- main.cpp
+++ main.cpp
@@ -37,38 +37,53 @@ void destroyTree(Node *node) {
     }
 }
 
-void openMpSum(Node *node, int *sum) {
-    if (node != NULL) {
-        int left_sum = 0, right_sum = 0;
-        #pragma omp task shared(left_sum)
-        openMpSum(node->left, &left_sum);
-        #pragma omp task shared(right_sum)
-        openMpSum(node->right, &right_sum);
-        #pragma omp taskwait
+void compute(int val) {
+    // perform some linear time operation
+    std::this_thread::sleep_for(std::chrono::milliseconds(val));
+}
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(node->val));
-        *sum = node->val + left_sum + right_sum;
+int sequentialSum(Node *node) {
+    if (node == NULL) {
+        return 0;
+    }
+    int sum_children = sequentialSum(node->left) + sequentialSum(node->right);
+    compute(node->val);
+    return node->val + sum_children;
+}
+
+void openMpSum(Node *node, int *sum) {
+    if (node == NULL) {
+        *sum = 0;
         return;
     }
-    *sum = 0;
+    int left_sum = 0, right_sum = 0;
+
+    #pragma omp task shared(left_sum)   // parallelize one subtree
+    openMpSum(node->left, &left_sum);
+
+    openMpSum(node->right, &right_sum);
+    #pragma omp taskwait
+
+    compute(node->val);
+    *sum = node->val + left_sum + right_sum;
 }
 
 void parallelSum(Node *node, int depth, int *sum) {
-    if (node != NULL) {
-        int left_sum = 0, right_sum = 0;
-        if (std::pow(2, depth) < NUM_THREADS) {
-            std::thread left (parallelSum, node->left, depth + 1, &left_sum);
-            parallelSum(node->right, depth + 1, &right_sum);
-            left.join();
-        } else {
-            parallelSum(node->left, depth + 1, &left_sum);
-            parallelSum(node->right, depth + 1, &right_sum);
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(node->val));
-        *sum = node->val + left_sum + right_sum;
+    if (node == NULL) {
+        *sum = 0;
         return;
     }
-    *sum = 0;;
+    int left_sum = 0, right_sum = 0;
+    if (std::pow(2, depth) < NUM_THREADS) {
+        std::thread left (parallelSum, node->left, depth + 1, &left_sum);
+        parallelSum(node->right, depth + 1, &right_sum);
+        left.join();
+    } else {
+        parallelSum(node->left, depth + 1, &left_sum);
+        parallelSum(node->right, depth + 1, &right_sum);
+    }
+    compute(node->val);
+    *sum = node->val + left_sum + right_sum;
 }
 
 double wall_time_ms() {
@@ -77,7 +92,7 @@ double wall_time_ms() {
     return 1000.0 * time.tv_sec + 0.001 * time.tv_usec;
 }
 
-int main() {
+int main(int argc, char **argv) {
 
     omp_set_num_threads(NUM_THREADS);
 
@@ -97,7 +112,13 @@ int main() {
         << " in " << elapsed << " ms" << std::endl;
 
     start = wall_time_ms();
-    int sum;
+    int sum = sequentialSum(root);
+    elapsed = wall_time_ms() - start;
+
+    std::cout << "sequentialSum completed in " << elapsed << "ms" << std::endl;
+    std::cout << "Sum: " << sum << std::endl;
+
+    start = wall_time_ms();
     #pragma omp parallel
     {
         #pragma omp single
